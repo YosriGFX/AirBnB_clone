3e64fc6b380014c193d7fd6d6556b43dea119247
YosriGFX <YosriG1997@gmail.com>
2020-02-17 23:30:34 +0100

    [init] -  <YosriGFX> 

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,22 @@ from datetime import datetime
 
 class BaseModel:
     '''BaseModel Class'''
+    forme = "%Y-%m-%dT%H:%M:%S.%f"
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''__init___'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(
+                                                            value,
+                                                            BaseModel.forme
+                                                        )
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         '''Return String'''
--- test_base_model.py
+++ test_base_model.py
@@ -4,25 +4,26 @@ from models.base_model import BaseModel
 my_model = BaseModel()
 my_model.name = "Holberton"
 my_model.my_number = 89
+print(my_model.id)
 print(my_model)
-my_model.save()
+print(type(my_model.created_at))
 
-print("\n___\n")
-
-print(my_model)
-
-print("\n___\n")
+print("\n\n--\n\n")
 
 my_model_json = my_model.to_dict()
-
 print(my_model_json)
-
-print("\n___\n")
-
 print("JSON of my_model:")
-
-print("\n___\n")
-
-
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("\n\n--\n\n")
+
+my_new_model = BaseModel(**my_model_json)
+
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("\n\n--\n\n")
+
+print(my_model is my_new_model)
