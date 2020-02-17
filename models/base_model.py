9cedaeb6b215d55f68a5fc896bd7cad6fa5d26a9
YosriGFX <YosriG1997@gmail.com>
2020-02-17 22:30:50 +0100

    [init] - Yosri

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,13 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
         '''Return String'''
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
+        return ("[{:s}] ({:s}) {}".format(
+                                            self.__class__.__name__,
+                                            self.id,
+                                            self.__dict__
+                                        ))
 
     def save(self):
         '''Save function'''
@@ -28,4 +30,4 @@ class BaseModel:
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         self.__dict__['__class__'] = self.__class__.__name__
-        return  (self.__dict__)
+        return (self.__dict__)
