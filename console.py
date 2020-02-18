8114b61db72da4ffdba5ab77ed6619e47c9aba63
YosriGFX <YosriG1997@gmail.com>
2020-02-18 14:32:01 +0100

    [init] -  <YosriGFX> 

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''console file'''
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''Class HBNB Command'''
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        '''quit'''
+        return True
+
+    def help_quit(self):
+        '''HELP_quit'''
+        print("Quit command to exit the program\n")
+
+    def do_EOF(self, line):
+        '''EOF'''
+        return True
+
+    def help_EOF(self):
+        '''HELP_EOF'''
+        print("CTRL+D command to exit the program\n")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
