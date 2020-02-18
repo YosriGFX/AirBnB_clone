7c94c135d685150f6d1c7672503c472862446b83
YosriGFX <YosriG1997@gmail.com>
2020-02-18 12:23:21 +0100

    [init] -  <YosriGFX> 

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,50 @@
-#!/usr/bin/env python3
-'''Base Model file'''
+#!/usr/bin/python3
+"""
+Define: BaseModel Class
+"""
 import uuid
-import models
 from datetime import datetime
+import models
+
+
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    '''BaseModel Class'''
-    forme = "%Y-%m-%dT%H:%M:%S.%f"
-
+    """
+    """
     def __init__(self, *args, **kwargs):
-        '''__init___'''
+        """
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(
-                                                            value,
-                                                            BaseModel.forme
-                                                        )
+                    self.__dict__[key] = datetime.strptime(value, time_format)
                 else:
                     self.__dict__[key] = value
         else:
             models.storage.new(self)
-
+        
     def __str__(self):
-        '''Return String'''
-        return ("[{:s}] ({:s}) {}".format(
-                                            self.__class__.__name__,
-                                            self.id,
-                                            self.__dict__
-                                        ))
+        """
+        """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        '''Save function'''
+        """
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
+
     def to_dict(self):
-        '''To dictionary function'''
-        new_dic = self.__dict__.copy()
-        new_dic['created_at'] = self.created_at.isoformat()
-        new_dic['updated_at'] = self.updated_at.isoformat()
-        new_dic['__class__'] = self.__class__.__name__
-        return (new_dic)
+        """
+        """
+        cl_dict = self.__dict__.copy()
+        cl_dict["created_at"] = self.created_at.isoformat()
+        cl_dict["updated_at"] = self.updated_at.isoformat()
+        cl_dict["__class__"] = self.__class__.__name__
+        return cl_dict
