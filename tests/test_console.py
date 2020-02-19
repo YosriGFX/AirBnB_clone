2106a47a37b90af3950d46ffc347e80613e6611c
YosriGFX <YosriG1997@gmail.com>
2020-02-19 22:25:54 +0100

    [init] -  <YosriGFX> 

--- tests/test_console.py
+++ tests/test_console.py
@@ -45,6 +45,19 @@ class TestConsole(unittest.TestCase):
         p = style.check_files(["console.py"])
         self.assertEqual(p.total_errors, 0, 'fix Pep8')
 
+    def test_docstrings_in_console(self):
+        '''checking for docstrings'''
+        self.assertIsNotNone(console.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.default.__doc__)
+
     def test_emptyline(self):
         '''Test empty line input'''
         with patch('sys.stdout', new=StringIO()) as f:
@@ -71,6 +84,8 @@ class TestConsole(unittest.TestCase):
             self.consol.onecmd("create User")
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("all User")
+            self.assertEqual(
+                '["[User', f.getvalue()[:7])
 
     def test_show(self):
         '''Test show command inpout'''
@@ -117,6 +132,7 @@ class TestConsole(unittest.TestCase):
             self.assertEqual("** class doesn't exist **\n", f.getvalue())
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("all State")
+            self.assertEqual('[]\n', f.getvalue())
 
     def test_update(self):
         '''Test update command inpout'''
@@ -148,7 +164,6 @@ class TestConsole(unittest.TestCase):
             self.consol.onecmd("update User " + my_id + " Name")
             self.assertEqual(
                 "** value missing **\n", f.getvalue())
-
 
 if __name__ == "__main__":
     '''__name___'''
