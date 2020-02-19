853277de0ccca6d328a23542d24a663f69753779
YosriGFX <YosriG1997@gmail.com>
2020-02-19 16:20:14 +0100

    [init] -  <YosriGFX> 

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,6 @@
 from models.base_model import BaseModel
 
 
-class Amenty(BaseModel):
+class Amenity(BaseModel):
 
     name = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 '''File Storage file'''
-from models.base_model import BaseModel
 import json
 from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+from models.base_model import BaseModel
 
 
 class FileStorage():
